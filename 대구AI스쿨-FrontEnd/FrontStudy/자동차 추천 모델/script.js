5d0b1f17f686ec30288ed09e5e708aa087af079a
hanseungjune <92035406+hanseungjune@users.noreply.github.com>
2022-05-03 15:32:11 +0900

    대구AI-FrontEnd

---
+++ 대구AI스쿨-FrontEnd/FrontStudy/자동차 추천 모델/script.js
@@ -0,0 +1,305 @@
+let size = document.querySelectorAll(".size");
+let fuel = document.querySelectorAll(".fuel");
+let brand = document.querySelectorAll(".brand");
+let i, j, k;
+let execute;
+
+function choice() {
+    for (i = 1; i < size.length; i++) {
+        var car_size = size[i];
+        if (car_size == 1) {
+            for (j = 1; j < fuel.length; j++) {
+                var car_fuel = fuel[j];
+                if (car_fuel == 1) {
+                    for (k = 1; k < brand.length; k++) {
+                        var car_brand = brand[k];
+                        if (car_brand == 1) {
+                            execute = document.write("https://auto.danawa.com/newcar/?&page=1");
+                            return execute;
+                        }
+                        else if (car_brand == 2) {
+                            window.open("");
+                        }
+                        else if (car_brand == 3) {
+                            window.open("");
+                        }
+                        else if (car_brand == 4) {
+                            window.open("");
+                        }
+                    }
+                }
+                else if (car_fuel == 2) {
+                    for (k = 1; k < brand.length; k++) {
+                        var car_brand = brand[k];
+                        if (car_brand == 1) {
+                            window.open("");
+                        }
+                        else if (car_brand == 2) {
+                            window.open("");
+                        }
+                        else if (car_brand == 3) {
+                            window.open("");
+                        }
+                        else if (car_brand == 4) {
+                            window.open("");
+                        }
+                    }
+                }
+                else if (car_fuel == 3) {
+                    for (k = 1; k < brand.length; k++) {
+                        var car_brand = brand[k];
+                        if (car_brand == 1) {
+                            window.open("");
+                        }
+                        else if (car_brand == 2) {
+                            window.open("");
+                        }
+                        else if (car_brand == 3) {
+                            window.open("");
+                        }
+                        else if (car_brand == 4) {
+                            window.open("");
+                        }
+                    }
+                }
+                else if (car_fuel == 4) {
+                    for (k = 1; k < brand.length; k++) {
+                        var car_brand = brand[k];
+                        if (car_brand == 1) {
+                            window.open("");
+                        }
+                        else if (car_brand == 2) {
+                            window.open("");
+                        }
+                        else if (car_brand == 3) {
+                            window.open("");
+                        }
+                        else if (car_brand == 4) {
+                            window.open("");
+                        }
+                    }
+                }
+            }
+        }
+        else if (car_size == 2) {
+            for (j = 1; j < fuel.length; j++) {
+                var car_fuel = fuel[j];
+                if (car_fuel == 1) {
+                    for (k = 1; k < brand.length; k++) {
+                        var car_brand = brand[k];
+                        if (car_brand == 1) {
+                            window.open("");
+                        }
+                        else if (car_brand == 2) {
+                            window.open("");
+                        }
+                        else if (car_brand == 3) {
+                            window.open("");
+                        }
+                        else if (car_brand == 4) {
+                            window.open("");
+                        }
+                    }
+                }
+                else if (car_fuel == 2) {
+                    for (k = 1; k < brand.length; k++) {
+                        var car_brand = brand[k];
+                        if (car_brand == 1) {
+                            window.open("");
+                        }
+                        else if (car_brand == 2) {
+                            window.open("");
+                        }
+                        else if (car_brand == 3) {
+                            window.open("");
+                        }
+                        else if (car_brand == 4) {
+                            window.open("");
+                        }
+                    }
+                }
+                else if (car_fuel == 3) {
+                    for (k = 1; k < brand.length; k++) {
+                        var car_brand = brand[k];
+                        if (car_brand == 1) {
+                            window.open("");
+                        }
+                        else if (car_brand == 2) {
+                            window.open("");
+                        }
+                        else if (car_brand == 3) {
+                            window.open("");
+                        }
+                        else if (car_brand == 4) {
+                            window.open("");
+                        }
+                    }
+                }
+                else if (car_fuel == 4) {
+                    for (k = 1; k < brand.length; k++) {
+                        var car_brand = brand[k];
+                        if (car_brand == 1) {
+                            window.open("");
+                        }
+                        else if (car_brand == 2) {
+                            window.open("");
+                        }
+                        else if (car_brand == 3) {
+                            window.open("");
+                        }
+                        else if (car_brand == 4) {
+                            window.open("");
+                        }
+                    }
+                }
+            }
+        }
+        else if (car_size == 3) {
+            for (j = 1; j < fuel.length; j++) {
+                var car_fuel = fuel[j];
+                if (car_fuel == 1) {
+                    for (k = 1; k < brand.length; k++) {
+                        var car_brand = brand[k];
+                        if (car_brand == 1) {
+                            window.open("");
+                        }
+                        else if (car_brand == 2) {
+                            window.open("");
+                        }
+                        else if (car_brand == 3) {
+                            window.open("");
+                        }
+                        else if (car_brand == 4) {
+                            window.open("");
+                        }
+                    }
+                }
+                else if (car_fuel == 2) {
+                    for (k = 1; k < brand.length; k++) {
+                        var car_brand = brand[k];
+                        if (car_brand == 1) {
+                            window.open("");
+                        }
+                        else if (car_brand == 2) {
+                            window.open("");
+                        }
+                        else if (car_brand == 3) {
+                            window.open("");
+                        }
+                        else if (car_brand == 4) {
+                            window.open("");
+                        }
+                    }
+                }
+                else if (car_fuel == 3) {
+                    for (k = 1; k < brand.length; k++) {
+                        var car_brand = brand[k];
+                        if (car_brand == 1) {
+                            window.open("");
+                        }
+                        else if (car_brand == 2) {
+                            window.open("");
+                        }
+                        else if (car_brand == 3) {
+                            window.open("");
+                        }
+                        else if (car_brand == 4) {
+                            window.open("");
+                        }
+                    }
+                }
+                else if (car_fuel == 4) {
+                    for (k = 1; k < brand.length; k++) {
+                        var car_brand = brand[k];
+                        if (car_brand == 1) {
+                            window.open("");
+                        }
+                        else if (car_brand == 2) {
+                            window.open("");
+                        }
+                        else if (car_brand == 3) {
+                            window.open("");
+                        }
+                        else if (car_brand == 4) {
+                            window.open("");
+                        }
+                    }
+                }
+            }
+        }
+        else if (car_size == 4) {
+            for (j = 1; j < fuel.length; j++) {
+                var car_fuel = fuel[j];
+                if (car_fuel == 1) {
+                    for (k = 1; k < brand.length; k++) {
+                        var car_brand = brand[k];
+                        if (car_brand == 1) {
+                            window.open("");
+                        }
+                        else if (car_brand == 2) {
+                            window.open("");
+                        }
+                        else if (car_brand == 3) {
+                            window.open("");
+                        }
+                        else if (car_brand == 4) {
+                            window.open("");
+                        }
+                    }
+                }
+                else if (car_fuel == 2) {
+                    for (k = 1; k < brand.length; k++) {
+                        var car_brand = brand[k];
+                        if (car_brand == 1) {
+                            window.open("");
+                        }
+                        else if (car_brand == 2) {
+                            window.open("");
+                        }
+                        else if (car_brand == 3) {
+                            window.open("");
+                        }
+                        else if (car_brand == 4) {
+                            window.open("");
+                        }
+                    }
+                }
+                else if (car_fuel == 3) {
+                    for (k = 1; k < brand.length; k++) {
+                        var car_brand = brand[k];
+                        if (car_brand == 1) {
+                            window.open("");
+                        }
+                        else if (car_brand == 2) {
+                            window.open("");
+                        }
+                        else if (car_brand == 3) {
+                            window.open("");
+                        }
+                        else if (car_brand == 4) {
+                            window.open("");
+                        }
+                    }
+                }
+                else if (car_fuel == 4) {
+                    for (k = 1; k < brand.length; k++) {
+                        var car_brand = brand[k];
+                        if (car_brand == 1) {
+                            window.open("");
+                        }
+                        else if (car_brand == 2) {
+                            window.open("");
+                        }
+                        else if (car_brand == 3) {
+                            window.open("");
+                        }
+                        else if (car_brand == 4) {
+                            window.open("");
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
